b9dbeec9836653f87ccbec47eda3bf41d1c47e5b
sandyacvedo <sandyacevedo270@gmail.com>
2022-04-21 01:18:28 -0400

    commit build and changes
    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { ThemeProvider } from "@mui/material";
 import { darkMode, lightMode } from "./ui/styles/styles";
@@ -9,6 +9,7 @@ import PrimarySearchAppBar from "./ui/components/NavBar/NavBar";
 import Home from "./ui/screens/Home/Home";
 import DetailPodcast from './ui/screens/DetailPodcast/DetailPodcast'
 import Search from './ui/screens/Search/Search'
+
 function App() {
 
   const { darkTheme } = useSelector((state) => state.theme);
